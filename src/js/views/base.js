029fe0212ad04a08c6513fa716407e466638851b
Marius-De-Allie <spectre4300s@yahoo.com>
2020-02-03 07:06:01 -0500

    Correct typos in .src/js/views/base.js file

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -3,7 +3,7 @@ const domElements = {
     searchInput: document.querySelector('.search__field'),
     searchForm: document.querySelector('.search'),
     searchResultList: document.querySelector('.results__list'),
-    resultsElement: document.querySecltor('.results')
+    resultsElement: document.querySelector('.results')
 };
 
 const renderLoader = parent => {
@@ -14,7 +14,7 @@ const renderLoader = parent => {
             </svg>
         </div>
     `;
-    parent.insertAdjecentHTML('afterbegin', loader);
+    parent.insertAdjacentHTML('afterbegin', loader);
 };
 
 export {domElements, renderLoader};

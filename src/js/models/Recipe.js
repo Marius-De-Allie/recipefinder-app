3484a71139856377fccd94257cb3f1d64d574432
Marius-De-Allie <spectre4300s@yahoo.com>
2020-02-04 19:11:48 -0500

    Declare getRecipe method within Recipe class which makes an async call to recipe search api

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -4,4 +4,11 @@ class Recipe {
     constructor(id) {
         this.id = id;
     }
+    async getRecipe() {
+        try {
+            const result = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
+        } catch(e) {
+            console.log(error);
+        }
+    }
 }

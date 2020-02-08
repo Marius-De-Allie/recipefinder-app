3126bf1a4545705943551a3bb242827b064c01ed
Marius-De-Allie <spectre4300s@yahoo.com>
2020-02-07 19:38:10 -0500

    Declare addLike method on Likes class within Likes.js file

--- src/js/models/Likes.js
+++ src/js/models/Likes.js
@@ -2,6 +2,16 @@ class Likes {
     constructor() {
         this.likes = [];
     }
+    addLike(id, title. author, image) {
+        const like = {
+            id: id,
+            title: title,
+            author: author,
+            image: image
+        };
+        this.likes.push(like);
+        return like;
+    }
 }
 
 export default Likes;

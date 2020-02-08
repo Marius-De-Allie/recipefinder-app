f817af0c8f529034412d9ad702a274cb085f89e6
Marius-De-Allie <spectre4300s@yahoo.com>
2020-02-07 22:22:19 -0500

    Declare renderLike function in likesView.js file

--- src/js/views/likesView.js
+++ src/js/views/likesView.js
@@ -13,4 +13,20 @@ const toggleLikeMenu = numLikes => {
     }
 };
 
+const renderLike = like => {
+    const markup = `
+        <li>
+            <a class="likes__link" href="#${like.id}">
+                <figure class="likes__fig">
+                    <img src="${like.image}" alt="${like.title}">
+                </figure>
+                <div class="likes__data">
+                    <h4 class="likes__name">${like.title}</h4>
+                    <p class="likes__author">${like.author}</p>
+                </div>
+            </a>
+        </li>
+    `;
+}
+
 export {toggleLike, toggleLikeMenu};

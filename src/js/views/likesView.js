7dbb596a2dd3dec9c7359a5019b290fe639ffd8b
Marius-De-Allie <spectre4300s@yahoo.com>
2020-02-07 21:50:20 -0500

    Declare toggleLikeMenu function within likesView.js file

--- src/js/views/likesView.js
+++ src/js/views/likesView.js
@@ -5,4 +5,12 @@ const toggleLike = isLiked => {
     document.querySelector('.recipe__love use').setAttribute('href', `img/icons.svg#${iconString}`);
 };
 
+const toggleLikeMenu = numLikes => {
+    if(numLikes > 0) {
+        domElements.likesMenu.classList.remove('hidden');
+    } else {
+        domElements.likesMenu.classList.add('hidden');
+    }
+};
+
 export {toggleLike};
